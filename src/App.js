4afd1de69c0b8c243ee1448a5a40b82fb6ae17a6
caren1 <czarnocki.w@wp.pl>
2020-09-17 22:33:02 +0200

    Inital header component, initialized dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Header } from './components/'
+// Header
+// TinderCard
+// Footer
 
 function App() {
   return (
     <div className="app">
-
+      <Header />
     </div>
   );
 }
