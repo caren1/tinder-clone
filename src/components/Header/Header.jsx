65c3fa3252a3ffdfa6adc8748f2935f30f6c43f1
caren1 <czarnocki.w@wp.pl>
2020-09-19 21:07:32 +0200

    completed Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import styles from './Header.module.css'
 
+import PersonIcon from '@material-ui/icons/Person'
+import IconButton from '@material-ui/core/IconButton'
+import ForumIcon from '@material-ui/icons/Forum'
+
 
 const Header = () => {
     return (
         <div className={styles.header}>
-            <h1>Header</h1>
+            <IconButton>
+                <PersonIcon fontSize="large" className={styles.header__icon}/>
+            </IconButton>
+            <img className={styles.header__logo}
+                src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+                alt="tinder-logo"/>
+            <IconButton>
+                <ForumIcon fontSize="large" className={styles.header__icon}/>
+            </IconButton>
         </div>
     )
 }
